f498bedecd4dd73c2c7af221ebfcf24ecee71690
Mason <mwallis5110@gmail.com>
2021-08-10 09:51:42 -0500

    Added time blocks

--- styles.js
+++ styles.js
@@ -1,7 +1,10 @@
 //Make time blocks in JS/JQuery
 var date = moment().format("MMMM Do YYYY");
-var hour = parseInt(moment(). format("HH"));
-$("#currentDay").text(date); 
+var hour = parseInt(moment().format("HH"));
+var formattedTime = "10:00:00"
+var time = parseInt(moment(formattedTime, "HH:mm:ss").format("HH:mm:ss"));
+$("#currentDay").text(date);
+$("#currentHour").text(time);
 //Add event listener for save buttons
 
 // for loop from start time to end time (*Use military time)
@@ -16,7 +19,7 @@ for (i = 9; i <= 18; i++); {
 
 $(".buttons").on("click", function() {
     var idSelect = $(this).attr("id");
-    var save =$(this).siblings("textarea").val();
+    var save = $(this).siblings("textarea").val();
     localStorage.setItem(idSelect, save);
 });
 
@@ -31,19 +34,20 @@ saveButtonEl.setAttribute('class', 'col-sm-1 saveBtn')
 
 for (let i = 0; i < hour; i++) {
     if (i < hour) {
-        descriptionEl.setAttribute('class', 'description past');
+        descriptionEl.setAttribute('class', 'event past');
     } else if (i === hour) {
-        descriptionEl.setAttribute('class', 'description present');
+        descriptionEl.setAttribute('class', 'event present');
     } else {
-        descriptionEl.setAttribute('class', 'description future')
-    }    
+        descriptionEl.setAttribute('class', 'event future')
+    }
 };
+
+
 // Way to select buttons (assign IDs), use IDs as what to save in local storage
 // Buttons to save in local storage
-    // Use DOM manipulation to give all buttons same class/different ID, 
-    //select all buttons via class and onClcik,
+// Use DOM manipulation to give all buttons same class/different ID, 
+//select all buttons via class and onClcik,
 // grab buttons and save to LS based on ID
 // Use i to grab text from local storage & put in correct box
 //assign classes in local storage, reference css 
 //.append everything
-
